49d4705e76daf4bc310e2007543b97ef342bbc16
Marco Ferreira <mw.marcowilliam@gmail.com>
2023-03-22 21:45:11 -0300

    style: ajustando tamanho da imagem

--- components/ui/BannerCarousel.tsx
+++ components/ui/BannerCarousel.tsx
@@ -57,14 +57,14 @@ function BannerItem({ image, lcp }: { image: Banner; lcp?: boolean }) {
             fetchPriority={lcp ? "high" : "auto"}
             src={mobile}
             width={360}
-            height={600}
+            height={596}
           />
           <Source
             media="(min-width: 768px)"
             fetchPriority={lcp ? "high" : "auto"}
             src={desktop}
-            width={1440}
-            height={600}
+            width={1920}
+            height={880}
           />
           <img
             class="object-cover w-full sm:h-full"
