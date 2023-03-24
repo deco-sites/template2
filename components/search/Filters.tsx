966746d568c229fecacd1c873e27ccd1524f7aa0
Marco Ferreira <mw.marcowilliam@gmail.com>
2023-03-24 11:28:53 -0300

    feat: filter component mobile

--- components/search/Filters.tsx
+++ components/search/Filters.tsx
@@ -1,76 +1,112 @@
 import Text from "$store/components/ui/Text.tsx";
 import Avatar from "$store/components/ui/Avatar.tsx";
 import type {
-  Filter,
-  FilterToggle,
-  ProductListingPage,
+	Filter,
+	FilterToggle,
+	ProductListingPage,
 } from "deco-sites/std/commerce/types.ts";
+import { useSignal } from "https://esm.sh/v111/@preact/signals@1.0.3/X-ZS8q/dist/signals";
+import Button from "../ui/Button.tsx";
+import Icon from "../ui/Icon.tsx";
 
 interface Props {
-  filters: ProductListingPage["filters"];
+	filters: ProductListingPage["filters"];
 }
 
 const isToggle = (filter: Filter): filter is FilterToggle =>
-  filter["@type"] === "FilterToggle";
+	filter["@type"] === "FilterToggle";
 
 function FilterValues({ key, values }: FilterToggle) {
-  const flexDirection = key === "tamanho" || key === "cor"
-    ? "flex-row"
-    : "flex-col";
+	const flexDirection =
+		key === "tamanho" || key === "cor" ? "flex-row" : "flex-col";
 
-  return (
-    <ul class={`flex flex-wrap gap-2 ${flexDirection}`}>
-      {values.map(({ label, value, url, selected, quantity }) => {
-        if (key === "cor") {
-          return (
-            <a href={url}>
-              <Avatar
-                // deno-lint-ignore no-explicit-any
-                content={value as any}
-                disabled={selected}
-                variant="color"
-              />
-            </a>
-          );
-        }
+	return (
+		<ul class={`flex flex-wrap gap-2 ${flexDirection}`}>
+			{values.map(({ label, value, url, selected, quantity }) => {
+				if (key === "cor") {
+					return (
+						<a
+							href={url}
+							class="p-[1px] border-2 border-gray-secondary flex items-center justify-center rounded overflow-hidden"
+						>
+							<Avatar
+								// deno-lint-ignore no-explicit-any
+								content={value as any}
+								disabled={selected}
+								variant="color"
+							/>
+						</a>
+					);
+				}
 
-        if (key === "tamanho") {
-          return (
-            <a href={url}>
-              <Avatar
-                content={label}
-                disabled={selected}
-                variant="abbreviation"
-              />
-            </a>
-          );
-        }
+				if (key === "tamanho") {
+					return (
+						<a href={url}>
+							<Avatar
+								content={label}
+								disabled={selected}
+								variant="abbreviation"
+							/>
+						</a>
+					);
+				}
 
-        return (
-          <a href={url} class="flex items-center gap-2">
-            <input type="checkbox" checked={selected} class="hidden" />
-            <Text variant="caption">{label}</Text>
-            <Text tone="subdued" variant="caption">
-              ({quantity})
-            </Text>
-          </a>
-        );
-      })}
-    </ul>
-  );
+				return (
+					<a href={url} class="flex items-center gap-2">
+						<input type="checkbox" checked={selected} class="hidden" />
+						<Text variant="caption">{label}</Text>
+						<Text tone="subdued" variant="caption">
+							({quantity})
+						</Text>
+					</a>
+				);
+			})}
+		</ul>
+	);
+}
+
+function FilterItem(filter: FilterToggle) {
+	const open = useSignal(false || filter.key === "cor");
+
+	return (
+		<li class="flex flex-col">
+			<div
+				class="flex justify-between items-center w-full py-2"
+				onClick={() => (open.value = !open.value)}
+			>
+				<Text variant="body" class="font-bold">
+					{filter.label}
+				</Text>
+				<Button variant="icon">
+					<Icon
+						class={open.value === true ? "hidden" : "block"}
+						id="Plus"
+						height={20}
+						width={20}
+						strokeWidth={1.5}
+					/>
+					<Icon
+						class={open.value === true ? "block" : "hidden"}
+						id="Minus"
+						height={20}
+						width={20}
+						strokeWidth={1.5}
+					/>
+				</Button>
+			</div>
+			<div class={`${open.value === true ? "flex" : "hidden"}`}>
+				<FilterValues {...filter} />
+			</div>
+		</li>
+	);
 }
 
 export default function Filters({ filters }: Props) {
-  return (
-    <ul class="flex flex-col gap-6 p-4">
-      {filters
-        .filter(isToggle)
-        .map((filter) => (
-          <li class="flex flex-col gap-4">
-            <Text variant="body">{filter.label}</Text>
-            <FilterValues {...filter} />
-          </li>
-        ))}
-    </ul>
-  );
+	return (
+		<ul class="flex flex-col gap-2 p-4">
+			{filters.filter(isToggle).map((filter) => (
+				<FilterItem {...filter} />
+			))}
+		</ul>
+	);
 }
--- components/search/Sort.tsx
+++ components/search/Sort.tsx
@@ -2,54 +2,99 @@ import { useMemo } from "preact/hooks";
 import Text from "$store/components/ui/Text.tsx";
 import Icon from "$store/components/ui/Icon.tsx";
 import type { JSX } from "preact";
+import Button from "../ui/Button.tsx";
+import { useSignal } from "https://esm.sh/v111/@preact/signals@1.0.3/X-ZS8q/dist/signals";
 
 const SORT_QUERY_PARAM = "sort";
 
 // TODO: The search query should also be from a commerce schema
 const options = [
-  { value: "", label: "Relevância" },
-  { value: "price:desc", label: "Maior Preço" },
-  { value: "price:asc", label: "Menor Preço" },
-  { value: "orders:desc", label: "Mais Pedidos" },
-  { value: "name:asc", label: "Nome (A -> Z)" },
-  { value: "name:desc", label: "Nome (Z -> A)" },
-  { value: "release:desc", label: "Lançamentos" },
-  { value: "discount:desc", label: "Maior Desconto" },
+	// { value: "", label: "Relevância" },
+	{ value: "price:desc", label: "Maior Preço" },
+	{ value: "price:asc", label: "Menor Preço" },
+	{ value: "orders:desc", label: "Mais Pedidos" },
+	{ value: "name:asc", label: "Nome (A -> Z)" },
+	{ value: "name:desc", label: "Nome (Z -> A)" },
+	{ value: "release:desc", label: "Lançamentos" },
+	{ value: "discount:desc", label: "Maior Desconto" },
 ];
 
 const useSort = () =>
-  useMemo(() => {
-    const urlSearchParams = new URLSearchParams(window.location?.search);
-    return urlSearchParams.get(SORT_QUERY_PARAM) ?? "";
-  }, []);
+	useMemo(() => {
+		const urlSearchParams = new URLSearchParams(window.location?.search);
+		return urlSearchParams.get(SORT_QUERY_PARAM) ?? "";
+	}, []);
 
 // TODO: Replace with "search utils"
-const applySort = (e: JSX.TargetedEvent<HTMLSelectElement, Event>) => {
-  const urlSearchParams = new URLSearchParams(window.location.search);
+const applySort = (value: string) => {
+	const urlSearchParams = new URLSearchParams(window.location.search);
 
-  console.log(e.currentTarget.value);
-
-  urlSearchParams.set(SORT_QUERY_PARAM, e.currentTarget.value);
-  window.location.search = urlSearchParams.toString();
+	urlSearchParams.set(SORT_QUERY_PARAM, value);
+	window.location.search = urlSearchParams.toString();
 };
 
 function Sort() {
-  const sort = useSort();
+	const sort = useSort();
+	const open = useSignal(false);
 
-  return (
-    <select
-      id="sort"
-      name="sort"
-      onInput={applySort}
-      class="w-min h-[36px] px-1 rounded m-2 text-button font-button text-default hover:bg-hover cursor-pointer outline-none"
-    >
-      {options.map(({ value, label }) => (
-        <option key={value} value={value} selected={value === sort}>
-          <Text variant="caption">{label}</Text>
-        </option>
-      ))}
-    </select>
-  );
+	return (
+		<div class="flex-col gap-2 px-4">
+			{/* <select
+        id="sort"
+        name="sort"
+        onInput={applySort}
+        class="w-min h-[36px] px-1 rounded m-2 text-button font-button text-default hover:bg-hover cursor-pointer outline-none"
+      >
+        {options.map(({ value, label }) => (
+          <option key={value} value={value} selected={value === sort}>
+            <Text variant="caption">{label}</Text>
+          </option>
+        ))}
+      </select> */}
+			<div
+				class="flex justify-between items-center w-full"
+				onClick={() => (open.value = !open.value)}
+			>
+				<Text variant="body" class="font-bold">
+					Ordernar
+				</Text>
+				<Button variant="icon">
+					<Icon
+						class={open.value === true ? "hidden" : "block"}
+						id="Plus"
+						height={20}
+						width={20}
+						strokeWidth={1.5}
+					/>
+					<Icon
+						class={open.value === true ? "block" : "hidden"}
+						id="Minus"
+						height={20}
+						width={20}
+						strokeWidth={1.5}
+					/>
+				</Button>
+			</div>
+			<div class={`flex flex-col ${open.value === true ? "flex" : "hidden"}`}>
+				{options.map(({ value, label }) => (
+					<div
+						key={value}
+						value={value}
+						// selected={value === sort}
+						class="flex gap-1.5 h-6"
+            onClick={() => applySort(value)}
+					>
+						<div
+							class={`w-[14px] h-[15px] border rounded text-[12px] flex justify-center items-center pt-0.5`}
+						>
+							{value === sort && `✔`}
+						</div>
+						<Text variant="caption">{label}</Text>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default Sort;
