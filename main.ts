7518f4280bdb698a436b340bd3f40b953b2f4976
Lucis <luciannojunior@gmail.com>
2023-03-26 15:51:36 -0300

    Using remote std

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ import manifest from "./fresh.gen.ts";
 import { start } from "$fresh/server.ts";
 import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "./twind.config.ts";
-import prefetchPlugin from "prefetch";
+// import prefetchPlugin from "prefetch";
 import partytownPlugin from "partytown/mod.ts";
 
 await start(manifest, {
